917556ca95669889b776e4084b4972c24608e5ea
Davi Fernando Rosa <davir0514@gmail.com>
2021-05-20 20:23:02 -0300

    Arquivo

---
+++ Estruturaifelse.java
@@ -0,0 +1,32 @@
+
+/**
+ *
+ * @author Davi
+ */
+public class Estruturaifelse {
+    
+    //exemplo comando if e else
+    /**
+      A expressão (valor1>valor2) sempre retornará um valor lógico, ou seja, verdadeiro
+ou falso. Caso verdadeiro, o bloco de comandos abaixo da condição será
+executado, caso contrário, o bloco else será executado. Você não é obrigado a
+implementar o bloco else caso seu programa não necessite.
+     
+     */
+    
+    public static void main(String[]args){
+     int valor1 = 5;
+     int valor2 = 10;
+
+
+     
+     if (valor1 > valor2){
+     System.out.println("Valor 1 é maior que valor 2");
+     }
+     else{
+       System.out.println("Valor 2 é maior que valor 1");
+     }
+    
+    }
+    
+}
